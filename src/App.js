ee29279943ebeca3960a613087c9b68831f47305
Ashwin Singh <91330777+ashwinsingh87@users.noreply.github.com>
2022-08-22 22:43:04 +0530

    https://github.com/ashwinsingh87/imdb-clone.git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './components/header/Header'
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from './page/home/Home';
+import MovieList from './components/movieList/MovieList';
+import Movie from './page/movieDetail/movie';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Header/> 
+        <Routes>
+          <Route index element = {<Home/>}></Route>
+          <Route path='movie/:id' element = {<Movie/>}></Route>
+          <Route path='movies/:type' element = {<MovieList/>}></Route>
+          <Route path='/*' element = {<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
+     </div>
   );
-}
+};
 
 export default App;
