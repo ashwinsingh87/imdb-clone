68fdca31bb56a9f5f5a8f46b6f6374df0e63ba68
Ashwin Singh <rajputashwin87@gmail.com>
2022-09-21 23:55:08 +0300

    final maybe

--- src/page/movieDetail/movie.js
+++ src/page/movieDetail/movie.js
@@ -79,7 +79,7 @@ const Movie = () => {
           </div>
           <div className="movie__detailRightBottom">
             <div className="synopsisText">Synopsis</div>
-            <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
+            <div className="synopsisDescription">{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
           </div>
         </div>
       </div>
